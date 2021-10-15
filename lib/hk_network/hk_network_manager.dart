8b97221e195d64424a6c51c20af801637bb040a6
Bryan <hongjb@homeking365.com>
2021-10-15 15:34:08 +0800

    -

---
+++ lib/hk_network/hk_network_manager.dart
@@ -0,0 +1,12 @@
+library hk_flutter_base_test;
+
+/// A Calculator.
+class Calculator1 {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  void printHello() {
+    // ignore: avoid_print
+    print("hello");
+  }
+}
