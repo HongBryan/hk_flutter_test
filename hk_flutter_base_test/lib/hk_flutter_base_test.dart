72da51ce32a77d753b982158b2ab2f28c8184b2d
Bryan <hongjb@homeking365.com>
2021-10-15 14:39:03 +0800

    添加文件

---
+++ hk_flutter_base_test/lib/hk_flutter_base_test.dart
@@ -0,0 +1,12 @@
+library hk_flutter_base_test;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  void printHello() {
+    // ignore: avoid_print
+    print("hello");
+  }
+}
